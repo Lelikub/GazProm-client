0541283e8c5faf88e4713cc022fcd72bd8e79edb
Lelikub <ildar100900@bk.ru>
2023-08-02 11:00:07 +0300

    modified:   node_modules/.cache/.eslintcache  	modified:   node_modules/.cache/default-development/0.pack  	modified:   node_modules/.cache/default-development/1.pack  	modified:   node_modules/.cache/default-development/10.pack  	modified:   node_modules/.cache/default-development/11.pack  	modified:   node_modules/.cache/default-development/2.pack  	modified:   node_modules/.cache/default-development/3.pack  	modified:   node_modules/.cache/default-development/4.pack  	modified:   node_modules/.cache/default-development/5.pack  	modified:   node_modules/.cache/default-development/6.pack  	modified:   node_modules/.cache/default-development/7.pack  	modified:   node_modules/.cache/default-development/8.pack  	modified:   node_modules/.cache/default-development/9.pack  	modified:   node_modules/.cache/default-development/index.pack  	modified:   node_modules/.cache/default-development/index.pack.old  	modified:   node_modules/.package-lock.json  	modified:   package-lock.json  	modified:   package.json  	modified:   src/App.js

--- src/components/chimform.js
+++ src/components/chimform.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import React from "react";
+
+const baseUrl = "http://localhost:3001/chims";
 
 class ChimForm extends React.Component{
     chim ={}
@@ -16,12 +19,12 @@ class ChimForm extends React.Component{
         return(
             <form ref={(el) => this.myForm = el} target="_self">
                 <legend>Химические вещества</legend>
-                <input placeholder="Температура С" onChange={(e)=> this.setState({temp:e.target.value})} required type="number" min="298" max="1000" step="any"/>
+                <input placeholder="Температура K" onChange={(e)=> this.setState({temp:e.target.value})} required type="number" min="298" max="1000" step="any"/>
                 <legend>Состав в процентах</legend>
                 <input placeholder="Метан" onChange={(e)=> this.setState({methane:e.target.value})} type="number" min="0" max="100" step="any"/>
                 <input placeholder="Этан" onChange={(e)=> this.setState({ethane:e.target.value})} type="number" min="0" max="100" step="any"/>
                 <input placeholder="Пропан" onChange={(e)=> this.setState({propane:e.target.value})} type="number" min="0" max="100" step="any"/>
-                <button type='button'  onClick={() =>{
+                <button id="button1" type='button'  onClick={() =>{
 
                     this.chim ={
                         temp: this.state.temp,
@@ -33,12 +36,32 @@ class ChimForm extends React.Component{
                     let b = Number(this.chim.temp)
                     
                     if((a > 100 || a < 100) || (b > 1000 || b < 273)){
-                        
+                        function toggleText(button_id)  {
+                            var text = document.getElementById(button_id).firstChild;
+                            text.data = "Проверьте данные!"
+                         } 
+                         toggleText("button1")
+
                     }
                     
                     else {
-                        this.props.sChim(this.chim)
+                        //this.props.sChim(this.chim)
                         //this.myForm.reset()
+                        axios.post(baseUrl,
+                            {
+                            "tempr": this.chim.temp,
+                            "methane": this.chim.methane,
+                            "ethane": this.chim.ethane,
+                            "propane": this.chim.propane
+                        }, ).then((res)=> {
+                            function toggleText(button_id)  {
+                                var text = document.getElementById(button_id).firstChild;
+                                text.data = res.data + " Дж"
+                             }
+                            console.log(res.data)
+
+                            toggleText("button1")}) // ПЕренос в api с подключением
+
                     }
                 }
                 }>Результат?</button>
